ee4b9de3b445bcdee21dee97e1e2b2ccfd914f8f
tushar <tusharsukale3412@gmail.com>
2017-08-02 12:50:57 +0530

    commited tree22

---
+++ set/src/set/Linked.java
@@ -0,0 +1,21 @@
+package set;
+import java.util.*;
+
+public class Linked {
+	public static void main(String[] args) {
+		
+		LinkedHashSet<Integer> a = new LinkedHashSet<Integer>();
+		a.add(12);
+		a.add(13);
+		a.add(14);
+		a.add(15);
+		a.add(2333);
+		
+		for(Integer b:a)
+		{
+			System.out.println(b);
+			// git
+		}
+	}
+
+}
